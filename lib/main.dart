47ef8a83da2df249926f60925e3ed7942958ad51
OgdenMorrow <kachallashalom@gmail.com>
2022-10-29 00:41:07 +0100

    i add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:startup/firebase_options.dart';
 import 'package:startup/screen/login_screen.dart';
 import 'package:startup/screen/random_words.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(StartUpNames());
 }
 
